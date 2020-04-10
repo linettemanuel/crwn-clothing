6937188f980d9d6e6e461bb5d53fbba42065f191
linettemanuel <manuel.linette@gmail.com>
2020-04-10 18:41:51 +0200

    updated shop data with new data, updated cart utils to group items in cart

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import CustomButton from '../custom-button/custom-button.component'
 import './cart-dropdown.style.scss'
 
 const CartDropdown = () => (
     <div className="cart-dropdown">
-        <div className="cart-items"/>>
+        <div className="cart-items"/>
         <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
 )
--- src/redux/cart/card.types.js
+++ src/redux/cart/card.types.js
@@ -1,3 +1,4 @@
 export const CartActionTypes = {
-    TOGGLE_CART_HIDDEN : 'TOGGLE_ACTION_HIDDEN'
+    TOGGLE_CART_HIDDEN : 'TOGGLE_ACTION_HIDDEN',
+    ADD_ITEM: 'ADD_ITEM'
 }
