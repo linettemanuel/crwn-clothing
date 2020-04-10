195c737d0d61b7c06a887d3bc94af302b10f7d22
linettemanuel <manuel.linette@gmail.com>
2020-04-10 16:13:58 +0200

    Updated our app,signed user cannot redirect to signin

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch, Redirect } from 'react-router-dom'
 import { connect } from 'react-redux';
 
 import HomePage from "./pages/homepage/homepage.component"
@@ -44,16 +44,20 @@ class App extends Component {
         <Switch>
           <Route exact path="/" component={HomePage } />
           <Route path='/shop' component={ShopPage} />
-          <Route path='/signin' component={SignInAndSignUpPage} />
+          <Route exact path='/signin' render={() => this.props.currentUser ? (<Redirect to='/' />) : (<SignInAndSignUpPage/>)} />
         </Switch>
       </div>
     ) 
   }
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+})
+
 const mapDispachtToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
 
-export default connect(null, mapDispachtToProps)(App);
+export default connect(mapStateToProps, mapDispachtToProps)(App);
